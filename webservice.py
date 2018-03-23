d22dad620d6428bbe33fda407da34c0ce1a2aeae
VCM admin account <vcm@vcm-3483.vm.duke.edu>
2018-03-23 18:16:54 -0400

    Add error codes

--- webservice.py
+++ webservice.py
@@ -19,24 +19,28 @@ def storeHR():
 		t = s["time"]
 		try:
 			mmod.add_heart_rate(em, hr, t)
+			return 'Successfully added heart rate to user', 200
 		except:
 			try:
 				ag = s["age"]
 				mmod.create_user(em, ag, hr, t)
+				return 'Successfully created new user', 200
 			except:
-				print("To create new user, specify age")
+				return 'To create new user, specify age', 400
 	except:
-		print("Please enter user email, heart rate, and time")
+		return 'Please enter user email, heart rate, and time', 400
 		
 
 @app.route("/api/heart_rate/<user_email>", methods=["GET"])
 def returnHRs(user_email):
 	mmod.print_user(user_email)
+	return jsonify(user_email), 200
 
 
 @app.route("/api/heart_rate/average/<user_email>", methods=["GET"])
 def returnAvg(user_email):
 	mmod.avg_HR(user_email)
+	return jsonify(avg), 200
 
 
 @app.route("/api/heart_rate/interval_average", methods=["POST"])
@@ -46,8 +50,9 @@ def returnIntAvg():
 		em = s["email"]
 		t = s["time"]
 		mmod.int_avg_HR(em, t)
+		return jsonify(int_avg), 200
 	except:
-		print("Please specify user email and start time to calculate average HR")
+		return 'Please specify user email and start time to calculate average HR', 400
 
 
 
