b4f8e45ff5bcfb017e8dc0e748dfa6c56ee364ae
Stephanie Hatz <seh46@duke.edu>
2018-04-03 21:48:16 -0400

    Actually add findpatient component consisting of text field and button

---
+++ heart-rate-viewer/src/FindPatient.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+
+class FindPatient extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			"nameTextField": "",
+		}
+	}
+
+	onNameTextFieldChange = (event) => {
+		this.setState({"nameTextField": event.target.value});
+	}
+
+	onButtonClick = (event) => {
+		console.log(this.state.nameTextField);
+	}
+
+	render() {
+		return (
+			<div>
+				<TextField 
+					value={this.state.nameTextField}
+					onChange={this.onNameTextFieldChange}/>
+				<Button onClick={this.onButtonClick}>
+					Log text field data.
+				</Button>
+				{this.state.nameTextField /*show the current nameTextField state here in the browser */} 
+			</div>
+		);
+	}
+}
+
+export default FindPatient
