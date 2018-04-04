2d1f23c95faeffa6da35f51f8563a1a8ea3ac52c
Stephanie Hatz <seh46@duke.edu>
2018-04-04 01:01:34 -0400

    Put input user and get data in one component file

--- heart-rate-viewer/src/App.js
+++ heart-rate-viewer/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import FindPatient from './FindPatient.js';
 import FetchData from './FetchData.js';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <FindPatient />
 		<FetchData />
       </div>
     );
--- heart-rate-viewer/src/FetchData.js
+++ heart-rate-viewer/src/FetchData.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import axios from 'axios';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 
 class FetchData extends React.Component {
 	constructor() {
 		super();
 		this.state = {
 			"data": "blank",
-		};
-		this.state = {
-			"user": this.props.userEmail,
+			"nameTextField": "",
+			"nameToSearch": "",
 		};
 	}
 
+	onNameTextFieldChange = (event) => {
+		this.setState({"nameTextField": event.target.value});
+	}
+
+	onButtonClick = (event) => {
+		console.log(this.state.nameTextField);
+		this.setState({"nameToSearch": this.state.nameTextField})
+	}
+
 	DataFromServer = () => {
-		axios.get("http://67.159.95.29:5000/api/heart_rate/seh46@duke.edu").then( (response) => {
+		axios.get("http://67.159.95.29:5000/api/heart_rate/" + this.state.nameToSearch).then( (response) => {
 			console.log(response.status);
 			this.setState({"data": response.data});
 		});
@@ -21,8 +31,19 @@ class FetchData extends React.Component {
 
 	render() {
 		return (
-			<div onClick={this.DataFromServer}>
-				{this.state.data}
+			<div>
+				<div>
+					<TextField 
+						value={this.state.nameTextField}
+						onChange={this.onNameTextFieldChange}/>
+					<Button onClick={this.onButtonClick}>
+						Log text field data.
+					</Button>
+				</div>
+				<div onClick={this.DataFromServer}>
+					{this.state.nameToSearch}
+					{this.state.data}
+				</div>
 			</div>
 		)
 	}
