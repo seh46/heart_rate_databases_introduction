648d00f28552109226ea75b9c500a0c5de7275c7
Stephanie Hatz <seh46@duke.edu>
2018-04-04 00:16:41 -0400

    Add FetchData to App

--- heart-rate-viewer/src/App.js
+++ heart-rate-viewer/src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FindPatient from './FindPatient.js';
+import FetchData from './FetchData.js';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <FindPatient />
+		<FetchData />
       </div>
     );
   }
