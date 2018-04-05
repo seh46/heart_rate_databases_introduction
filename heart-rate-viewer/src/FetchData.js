f9fa6627acbc556abba24414070fe6aed23bed8b
Stephanie Hatz <seh46@duke.edu>
2018-04-04 22:39:39 -0400

    Could not call functions directly so try on change of user email object

--- heart-rate-viewer/src/FetchData.js
+++ heart-rate-viewer/src/FetchData.js
@@ -37,8 +37,8 @@ class FetchData extends React.Component {
 	render() {
 		return (
 			<div>
-				{this.DataFromServer()}
-				{this.FormatDataTable()}
+				this.props.userEmail.onchange={this.DataFromServer()};
+				this.props.userEmail.onchange={this.FormatDataTable()};
 				<Paper>
 					<Table>
 						<TableHead>
