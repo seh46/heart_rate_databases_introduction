9d7513fddcca145db239f0b02844a0ade2b114df
Stephanie Hatz <seh46@duke.edu>
2018-04-04 23:43:10 -0400

    Put datatable function call in {}

--- heart-rate-viewer/src/FetchData.js
+++ heart-rate-viewer/src/FetchData.js
@@ -81,7 +81,7 @@ class FetchData extends React.Component {
 							</TableRow>
 						</TableHead>
 						<TableBody>
-							this.dataTable(data)
+							{this.dataTable(data)}
 						</TableBody>
 					</Table>
 				</Paper>
